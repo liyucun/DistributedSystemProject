abfaf33cdc3450ea98475f654528d23ebcbb2768
yucun li <liyucun2012@gmail.com>
2015-10-09 17:18:13 -0400

    customer client tested

--- CustomerClient/src/com/client/CustomerClient.java
+++ CustomerClient/src/com/client/CustomerClient.java
@@ -13,17 +13,50 @@ import java.rmi.registry.Registry;
  */
 public class CustomerClient {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws RemoteException, NotBoundException{
-        Registry registry = LocateRegistry.getRegistry("localhost", Constant.BANK_TD_RMI_PORT);
-        CustomerInterface lookup = (CustomerInterface) registry.lookup(Constant.BANK_TD_RMI_ID);
-        System.out.println(lookup.openAccount("Bank Name", "First name", "Last name", 
-                "my email", "5144308601", "password"));
-        System.out.println(lookup.openAccount("Bank Name", "First name", "Last name", 
-                "my email", "5144308601", "password"));
         
+        CustomerClient client = new CustomerClient();
+        client.openAccount(Constant.BANK_TD_RMI_ID, "Yucun", "Li", "email@mail.ca", "514444", "123");
+        client.getLoan(Constant.BANK_TD_RMI_ID, 1+"", "123", 300);
+    }
+    
+    public void openAccount(String bank, String firstName, String lastName, 
+            String email, String phone, String password) throws RemoteException, NotBoundException{
+        Registry registry = null;
+        CustomerInterface lookup = null;
+        
+        if(bank.equals(Constant.BANK_SCOTIA_RMI_ID)){
+            registry = LocateRegistry.getRegistry("localhost", Constant.BANK_SCOTIA_RMI_PORT);
+            lookup = (CustomerInterface) registry.lookup(Constant.BANK_SCOTIA_RMI_ID);
+        }else if(bank.equals(Constant.BANK_BMO_RMI_ID)){
+            registry = LocateRegistry.getRegistry("localhost", Constant.BANK_BMO_RMI_PORT);
+            lookup = (CustomerInterface) registry.lookup(Constant.BANK_BMO_RMI_ID);
+        }else if(bank.equals(Constant.BANK_TD_RMI_ID)){
+            registry = LocateRegistry.getRegistry("localhost", Constant.BANK_TD_RMI_PORT);
+            lookup = (CustomerInterface) registry.lookup(Constant.BANK_TD_RMI_ID);
+        }
+        
+        System.out.println(lookup.openAccount(bank, firstName, lastName, email, phone, password));
+        
+    }
+    
+    public void getLoan(String bank, String accountNumber, String password, int loanAmount) 
+            throws RemoteException, NotBoundException{
+        Registry registry = null;
+        CustomerInterface lookup = null;
+        
+        if(bank.equals(Constant.BANK_SCOTIA_RMI_ID)){
+            registry = LocateRegistry.getRegistry("localhost", Constant.BANK_SCOTIA_RMI_PORT);
+            lookup = (CustomerInterface) registry.lookup(Constant.BANK_SCOTIA_RMI_ID);
+        }else if(bank.equals(Constant.BANK_BMO_RMI_ID)){
+            registry = LocateRegistry.getRegistry("localhost", Constant.BANK_BMO_RMI_PORT);
+            lookup = (CustomerInterface) registry.lookup(Constant.BANK_BMO_RMI_ID);
+        }else if(bank.equals(Constant.BANK_TD_RMI_ID)){
+            registry = LocateRegistry.getRegistry("localhost", Constant.BANK_TD_RMI_PORT);
+            lookup = (CustomerInterface) registry.lookup(Constant.BANK_TD_RMI_ID);
+        }
+        
+        System.out.println(lookup.getLoan(bank, accountNumber, password, loanAmount));
     }
     
 }
